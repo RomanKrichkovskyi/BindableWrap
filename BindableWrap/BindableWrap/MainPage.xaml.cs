fc3102b949fd624e91da4fbf98bdde357675142b
DESKTOP-KB22PM6\roman <rkrichkovsky@gmail.com>
2018-03-14 21:30:55 +0200

    Initial commit

---
+++ BindableWrap/BindableWrap/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BindableWrap
+{
+	public partial class MainPage : ContentPage
+	{
+        public List<string> ListName { get; set; }
+		public MainPage()
+		{
+			InitializeComponent();
+            ListName = new List<string> { "Joni", "Toni", "Archi", "Joni", "Tonieeee", "Archwei" };
+            StackL.ItemsSource = ListName;
+
+        }
+	}
+}
---
+++ BindableWrap/BindableWrap/WrapLayout.cs
@@ -0,0 +1,416 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using Xamarin.Forms;
+
+
+
+namespace BindableWrap
+{
+    /// <summary>
+    /// Bindabl Layout panel with wraping
+    /// </summary>
+    public partial class WrapLayout : Layout<View>
+    {
+        #region Set Bindable property
+
+
+    //    public static readonly BindableProperty ItemTemplateProperty = BindableProperty.Create((nameof(DataTemplate), typeof(DataTemplate), typeof(WrapLayout), p => p.ItemTemplate, BindingMode.OneWay, null, ItemTemplateChanged);
+
+        public static readonly BindableProperty ItemTemplateProperty = BindableProperty.Create<WrapLayout, DataTemplate>(p => p.ItemTemplate, default(DataTemplate), BindingMode.OneWay, null, ItemTemplateChanged);
+        public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create<WrapLayout, IEnumerable>(p => p.ItemsSource, default(IEnumerable<object>), BindingMode.TwoWay, null, ItemsSourceChanged);
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public DataTemplate ItemTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }
+
+        private static void ItemTemplateChanged(BindableObject bindable, DataTemplate oldValue, DataTemplate newValue)
+        {
+            var itemsLayout = (WrapLayout)bindable;
+
+            if (itemsLayout.ItemsSource != null)
+            {
+                itemsLayout.SetItems();
+            }
+        }
+
+        private static void ItemsSourceChanged(BindableObject bindable, IEnumerable oldValue, IEnumerable newValue)
+        {
+            var itemsLayout = (WrapLayout)bindable;
+
+            if (itemsLayout.ItemTemplate != null)
+            {
+                itemsLayout.SetItems();
+            }
+        }
+
+        protected virtual void SetItems()
+        {
+            Children.Clear();
+
+            if (ItemsSource == null)
+            {
+                ObservableSource = null;
+                return;
+            }
+
+            foreach (var item in ItemsSource)
+            {
+                var storage = this.ItemTemplate.CreateContent() as Element;
+                if (storage != null)
+                {
+                    storage.BindingContext = item;
+                }
+                var vi = storage as View;
+
+                Children.Add(vi);
+            }
+
+            var isObs = /*ItemsSource.GetType().IsGenericType &&*/ ItemsSource.GetType().GetGenericTypeDefinition() == typeof(ObservableCollection<>);
+            if (isObs)
+            {
+                ObservableSource = new ObservableCollection<object>(ItemsSource.Cast<object>());
+            }
+        }
+
+        ObservableCollection<object> _observableSource;
+        protected ObservableCollection<object> ObservableSource
+        {
+            get { return _observableSource; }
+            set
+            {
+                if (_observableSource != null)
+                {
+                    _observableSource.CollectionChanged -= CollectionChanged;
+                }
+                _observableSource = value;
+
+                if (_observableSource != null)
+                {
+                    _observableSource.CollectionChanged += CollectionChanged;
+                }
+            }
+        }
+
+        private void CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    {
+                        int index = e.NewStartingIndex;
+                        foreach (var item in e.NewItems)
+                            Children.Insert(index++, GetItemView(item));
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Move:
+                    {
+                        var item = ObservableSource[e.OldStartingIndex];
+                        Children.RemoveAt(e.OldStartingIndex);
+                        Children.Insert(e.NewStartingIndex, GetItemView(item));
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    {
+                        Children.RemoveAt(e.OldStartingIndex);
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                    {
+                        Children.RemoveAt(e.OldStartingIndex);
+                        Children.Insert(e.NewStartingIndex, GetItemView(ObservableSource[e.NewStartingIndex]));
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    Children.Clear();
+                    foreach (var item in ItemsSource)
+                        Children.Add(GetItemView(item));
+                    break;
+            }
+        }
+
+        public interface IObservableReadOnlyCollection<out T> : IEnumerable<T>, INotifyCollectionChanged
+        {
+            T this[int i] { get; }
+            int Count { get; }
+        }
+
+        public class ObservableReadOnlyCollection<T> : IObservableReadOnlyCollection<T>
+        {
+            public ObservableCollection<T> _inner;
+            public ObservableReadOnlyCollection(ObservableCollection<T> inner) { _inner = inner; }
+
+            public T this[int i] => _inner[i];
+            public int Count => _inner.Count;
+
+            public event NotifyCollectionChangedEventHandler CollectionChanged
+            {
+                add { _inner.CollectionChanged += value; }
+                remove { _inner.CollectionChanged -= value; }
+            }
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                return _inner.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return _inner.GetEnumerator();
+            }
+        }
+
+        protected virtual View GetItemView(object item)
+        {
+            var content = ItemTemplate.CreateContent();
+
+            var view = content as View;
+            if (view == null)
+                return null;
+
+            view.BindingContext = item;
+
+            return view;
+        }
+
+        #endregion
+
+
+
+        #region Wrap element
+
+        /// <summary>
+        /// Backing Storage for the Orientation property
+        /// </summary>
+        public static readonly BindableProperty OrientationProperty =
+            BindableProperty.Create(
+                nameof(Orientation),
+                typeof(StackOrientation),
+                typeof(WrapLayout),
+                StackOrientation.Vertical,
+                BindingMode.TwoWay,
+                null,
+                propertyChanged: (bindable, oldvalue, newvalue) => ((WrapLayout)bindable).OnSizeChanged());
+
+        /// <summary>
+        /// Orientation (Horizontal or Vertical)
+        /// </summary>
+        public StackOrientation Orientation
+        {
+            get { return (StackOrientation)this.GetValue(OrientationProperty); }
+            set { this.SetValue(OrientationProperty, value); }
+        }
+
+        /// <summary>
+        /// Backing Storage for the Spacing property
+        /// </summary>
+        public static readonly BindableProperty SpacingProperty =
+            BindableProperty.Create(
+                nameof(Spacing),
+                typeof(double),
+                typeof(WrapLayout),
+                (double)6,
+                propertyChanged: (bindable, oldvalue, newvalue) => ((WrapLayout)bindable).OnSizeChanged());
+
+        /// <summary>
+        /// Spacing added between elements (both directions)
+        /// </summary>
+        /// <value>The spacing.</value>
+        public double Spacing
+        {
+            get { return (double)this.GetValue(SpacingProperty); }
+            set { this.SetValue(SpacingProperty, value); }
+        }
+
+        /// <summary>
+        /// This is called when the spacing or orientation properties are changed - it forces
+        /// the control to go back through a layout pass.
+        /// </summary>
+        private void OnSizeChanged()
+        {
+            this.ForceLayout();
+        }
+
+        /// <summary>
+        /// This method is called during the measure pass of a layout cycle to get the desired size of an element.
+        /// </summary>
+        /// <param name="widthConstraint">The available width for the element to use.</param>
+        /// <param name="heightConstraint">The available height for the element to use.</param>
+        protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
+        {
+            if (WidthRequest > 0)
+                widthConstraint = Math.Min(widthConstraint, WidthRequest);
+            if (HeightRequest > 0)
+                heightConstraint = Math.Min(heightConstraint, HeightRequest);
+
+            double internalWidth = double.IsPositiveInfinity(widthConstraint) ? double.PositiveInfinity : Math.Max(0, widthConstraint);
+            double internalHeight = double.IsPositiveInfinity(heightConstraint) ? double.PositiveInfinity : Math.Max(0, heightConstraint);
+
+            return Orientation == StackOrientation.Vertical
+                ? DoVerticalMeasure(internalWidth, internalHeight)
+                    : DoHorizontalMeasure(internalWidth, internalHeight);
+        }
+
+        /// <summary>
+        /// Does the vertical measure.
+        /// </summary>
+        /// <returns>The vertical measure.</returns>
+        /// <param name="widthConstraint">Width constraint.</param>
+        /// <param name="heightConstraint">Height constraint.</param>
+        private SizeRequest DoVerticalMeasure(double widthConstraint, double heightConstraint)
+        {
+            int columnCount = 1;
+
+            double width = 0;
+            double height = 0;
+            double minWidth = 0;
+            double minHeight = 0;
+            double heightUsed = 0;
+
+            foreach (var item in Children)
+            {
+                var size = item.Measure(widthConstraint, heightConstraint);
+                width = Math.Max(width, size.Request.Width);
+
+                var newHeight = height + size.Request.Height + Spacing;
+                if (newHeight > heightConstraint)
+                {
+                    columnCount++;
+                    heightUsed = Math.Max(height, heightUsed);
+                    height = size.Request.Height;
+                }
+                else
+                    height = newHeight;
+
+                minHeight = Math.Max(minHeight, size.Minimum.Height);
+                minWidth = Math.Max(minWidth, size.Minimum.Width);
+            }
+
+            if (columnCount > 1)
+            {
+                height = Math.Max(height, heightUsed);
+                width *= columnCount;  // take max width
+            }
+
+            return new SizeRequest(new Size(width, height), new Size(minWidth, minHeight));
+        }
+
+        /// <summary>
+        /// Does the horizontal measure.
+        /// </summary>
+        /// <returns>The horizontal measure.</returns>
+        /// <param name="widthConstraint">Width constraint.</param>
+        /// <param name="heightConstraint">Height constraint.</param>
+        private SizeRequest DoHorizontalMeasure(double widthConstraint, double heightConstraint)
+        {
+            int rowCount = 1;
+
+            double width = 0;
+            double height = 0;
+            double minWidth = 0;
+            double minHeight = 0;
+            double widthUsed = 0;
+
+            foreach (var item in Children)
+            {
+                var size = item.Measure(widthConstraint, heightConstraint);
+                height = Math.Max(height, size.Request.Height);
+
+                var newWidth = width + size.Request.Width + Spacing;
+                if (newWidth > widthConstraint)
+                {
+                    rowCount++;
+                    widthUsed = Math.Max(width, widthUsed);
+                    width = size.Request.Width;
+                }
+                else
+                    width = newWidth;
+
+                minHeight = Math.Max(minHeight, size.Minimum.Height);
+                minWidth = Math.Max(minWidth, size.Minimum.Width);
+            }
+
+            if (rowCount > 1)
+            {
+                width = Math.Max(width, widthUsed);
+                height *= rowCount;  // take max height
+            }
+
+            return new SizeRequest(new Size(width, height), new Size(minWidth, minHeight));
+        }
+
+        /// <summary>
+        /// Positions and sizes the children of a Layout.
+        /// </summary>
+        /// <param name="x">A value representing the x coordinate of the child region bounding box.</param>
+        /// <param name="y">A value representing the y coordinate of the child region bounding box.</param>
+        /// <param name="width">A value representing the width of the child region bounding box.</param>
+        /// <param name="height">A value representing the height of the child region bounding box.</param>
+        protected override void LayoutChildren(double x, double y, double width, double height)
+        {
+            if (Orientation == StackOrientation.Vertical)
+            {
+                double colWidth = 0;
+                double yPos = y, xPos = x;
+
+                foreach (var child in Children.Where(c => c.IsVisible))
+                {
+                    var request = child.Measure(width, height);
+
+                    double childWidth = request.Request.Width;
+                    double childHeight = request.Request.Height;
+                    colWidth = Math.Max(colWidth, childWidth);
+
+                    if (yPos + childHeight > height)
+                    {
+                        yPos = y;
+                        xPos += colWidth + Spacing;
+                        colWidth = 0;
+                    }
+
+                    var region = new Rectangle(xPos, yPos, childWidth, childHeight);
+                    LayoutChildIntoBoundingRegion(child, region);
+                    yPos += region.Height + Spacing;
+                }
+            }
+            else
+            {
+                double rowHeight = 0;
+                double yPos = y, xPos = x;
+
+                foreach (var child in Children.Where(c => c.IsVisible))
+                {
+                    var request = child.Measure(width, height);
+
+                    double childWidth = request.Request.Width;
+                    double childHeight = request.Request.Height;
+                    rowHeight = Math.Max(rowHeight, childHeight);
+
+                    if (xPos + childWidth > width)
+                    {
+                        xPos = x;
+                        yPos += rowHeight + Spacing;
+                        rowHeight = 0;
+                    }
+
+                    var region = new Rectangle(xPos, yPos, childWidth, childHeight);
+                    LayoutChildIntoBoundingRegion(child, region);
+                    xPos += region.Width + Spacing;
+                }
+
+            }
+        }
+    }
+    #endregion
+}
